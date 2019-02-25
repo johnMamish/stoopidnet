0217ae8fb4acb36544d347211df4b887f0724bdf
John Mamish <john.mamish@maymobility.com>
2019-02-25 12:04:02 -0500

    backprop algorithm wip

--- src/stoopidnet.c
+++ src/stoopidnet.c
@@ -26,13 +26,67 @@ struct stoopidnet
     double** biases;
 };
 
+typedef struct stoopidnet_forward_prop_results
+{
+    /**
+     * a[0] contains input activations
+     */
+    double** a;
+
+    /**
+     * z[0] contains nothing, z[1] contains z for layer 1 (first hidden layer).
+     */
+    double** z;
+} stoopidnet_forward_prop_results_t;
+
 ////////////////////////////////////////////////////////////////
 // static helper function decls
 ////////////////////////////////////////////////////////////////
+
+static int maxidx(double* vec, int len)
+{
+    double maxval = vec[0];
+    double maxidx = 0;
+
+    for (int i = 1; i < len; i++) {
+        if (vec[i] > maxval) {
+            maxval = vec[i];
+            maxidx = i;
+        }
+    }
+
+    return maxidx;
+}
+
 /**
  * Calculates the sigmoid function for a given value.
  */
 static double sigmoid(double z);
+
+/**
+ * Calculates d sigmoid(z) / dz
+ */
+static double sigmoid_prime(double z);
+
+/**
+ * Returns an allocated list of numbers [0, n) that have been shuffled.
+ */
+static int* gen_shuffled_ints(int n);
+
+/**
+ *
+ */
+static void doubles_memset(double* d, int numel, double val);
+
+//static void stoopidnet_forward_prop_results_destroy(stoopidnet_t* net,
+//                                                    stoopidnet_forward_prop_results_t* res);
+
+/**
+ * Fundamentally does the same thing as eval, but stores some intermediate results that will be
+ * useful for calculating backprop.
+ */
+static stoopidnet_forward_prop_results_t* stoopidnet_forward_prop(stoopidnet_t* net,
+                                                                  double *input);
 
 
 stoopidnet_t* stoopidnet_create(uint32_t num_input_nodes)
@@ -345,7 +399,96 @@ void stoopidnet_train(stoopidnet_t* net,
                       double** inputs,
                       double** outputs)
 {
+    // shuffle training examples
+    int* shuffle = gen_shuffled_ints(n_inputs);
 
+    // setup empty arrays for accumulating average of gradients over training mini-batch
+    double** weight_grads = calloc(net->num_layers - 1, sizeof(double*));
+    double** bias_grads   = calloc(net->num_layers - 1, sizeof(double*));
+    for (int i = 0; i < net->num_layers - 1; i++) {
+        weight_grads[i] = malloc(net->layer_sizes[i] * net->layer_sizes[i + 1] * sizeof(double));
+        bias_grads[i] = malloc(net->layer_sizes[i + 1] * sizeof(double));
+    }
+
+    // do mini batches
+    double* grad_cost = malloc(net->layer_sizes[net->num_layers - 1] * sizeof(double));
+    for (int i = 0; i < n_inputs;) {
+        // reset gradient vectors
+        for (int j = 0; j < net->num_layers - 1; j++) {
+            doubles_memset(weight_grads[j], net->layer_sizes[j] * net->layer_sizes[j + 1], 0.0);
+            doubles_memset(bias_grads[j], net->layer_sizes[j + 1], 0.0);
+        }
+
+        // actual backprop happens here
+        // allocate array for layer-by-layer error
+        double** layer_error = calloc(net->num_layers, sizeof(double*));
+        for (int j = 1; j < net->num_layers; j++) {
+            layer_error[j] = malloc(net->layer_sizes[j] * sizeof(double));
+        }
+
+        for (int j = 0; j < params->batch_size; j++, i++) {
+            // first run network forward and cache z-values and a-values.
+            stoopidnet_forward_prop_results_t* fp = stoopidnet_forward_prop(net,
+                                                                            inputs[shuffle[i]]);
+
+            // backpropagate
+            // final layer is special case
+            int backprop_layer = net->num_layers - 1;
+            for (int k = 0; k < net->layer_sizes[net->num_layers - 1]; k++) {
+                layer_error[backprop_layer][k] = ((fp->a[backprop_layer][k] -
+                                                       (outputs[shuffle[i]])[k]) *
+                                                      sigmoid_prime(fp->a[backprop_layer][k]));
+            }
+            backprop_layer--;
+            for (;backprop_layer > 0; backprop_layer--) {
+                for (int k = 0; k < net->layer_sizes[backprop_layer]; k++) {
+                    layer_error[backprop_layer][k] = 0.;
+                    for (int l = 0; l < net->layer_sizes[backprop_layer - 1]; l++) {
+                        int idx = (k * net->layer_sizes[backprop_layer - 1]) + l;
+                        layer_error[backprop_layer][k] +=
+                            (net->weights[backprop_layer - 1][idx] *
+                             layer_error[backprop_layer + 1][k]);
+                    }
+                    layer_error[backprop_layer][k] *= sigmoid_prime(fp->z[backprop_layer][k]);
+                }
+            }
+
+            // add to gradient vectors.
+            for (int k = 1; k < net->num_layers; k++) {
+                for (int l = 0; l < net->layer_sizes[k]; l++) {
+                    bias_grads[k - 1][l] += layer_error[k][l];
+                    for (int m = 0; m < net->layer_sizes[k - 1]; m++) {
+                        int idx = (l * net->layer_sizes[k - 1]) + m;
+                        weight_grads[k - 1][idx] += fp->a[k - 1][m] * layer_error[k][l];
+                    }
+                }
+            }
+        }
+
+        // update network state with gradient.
+        double lrate = (params->learn_rate / ((double)params->batch_size));
+        for (int layer = 0; layer < net->num_layers - 1; layer++) {
+            for (int l_plus_1_idx = 0; l_plus_1_idx < net->layer_sizes[layer + 1]; l_plus_1_idx++) {
+                net->biases[layer][l_plus_1_idx] -= lrate * (bias_grads[layer][l_plus_1_idx]);
+                for (int l_idx = 0; l_idx < net->layer_sizes[layer]; l_idx++) {
+                    int widx = (l_plus_1_idx * net->layer_sizes[layer]) + l_idx;
+                    net->weights[layer][widx] -= lrate * (weight_grads[layer][widx]);
+                }
+            }
+        }
+
+        // print stats after training round.
+        int num_good = 0;
+        for (int j = 0; j < n_inputs; j++) {
+            double* output;
+            stoopidnet_evaluate(net, inputs[j], &output);
+
+            if(maxidx(output, 10) == (maxidx(inputs[j], 10))) {
+                num_good++;
+            }
+        }
+        printf("%i examples trained. %i / %i accuracy\n", i, num_good, n_inputs);
+    }
 }
 
 
@@ -355,4 +498,71 @@ void stoopidnet_train(stoopidnet_t* net,
 static double sigmoid(double z)
 {
     return 1. / (1. + exp(-z));
+}
+
+static double sigmoid_prime(double z)
+{
+    double enz = exp(-z);
+    return (enz / ((1 + enz) * (1 + enz)));
+}
+
+static int* gen_shuffled_ints(int n)
+{
+    int* array = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        array[i] = i;
+    }
+
+    if (n > 1) {
+        size_t i;
+	for (i = 0; i < n - 1; i++) {
+	  size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+	  int t = array[j];
+	  array[j] = array[i];
+	  array[i] = t;
+	}
+    }
+
+    return array;
+}
+
+static void doubles_memset(double* d, int numel, double val)
+{
+    for (int i = 0; i < numel; i++) {
+        d[i] = val;
+    }
+}
+
+static stoopidnet_forward_prop_results_t* stoopidnet_forward_prop(stoopidnet_t* net,
+                                                                  double *input)
+{
+    stoopidnet_forward_prop_results_t* results =
+        calloc(1, sizeof(stoopidnet_forward_prop_results_t));
+
+    results->a = calloc(net->num_layers, sizeof(double*));
+    results->z = calloc(net->num_layers, sizeof(double*));
+
+    results->a[0] = malloc(net->layer_sizes[0] * sizeof(double));
+
+    memcpy(results->a[0], input, sizeof(double) * net->layer_sizes[0]);
+    double *activiation_next = NULL;
+
+    for (int l = 1; l < net->num_layers; l++) {
+        results->z[l] = calloc(net->layer_sizes[l], sizeof(double));
+        results->a[l] = calloc(net->layer_sizes[l], sizeof(double));
+
+        // calculate next layer into activiation_next
+        for (int j = 0; j < net->layer_sizes[l]; j++) {
+            double accum = 0.;
+            for (int k = 0; k < net->layer_sizes[l - 1]; k++) {
+                int idx = (j * net->layer_sizes[l - 1]) + k;
+                accum += (net->weights[l - 1])[idx] * results->a[l - 1][k];
+            }
+            results->z[l][j] = accum;
+            results->a[l][j] = sigmoid(accum - (net->biases[l - 1])[j]);
+        }
+    }
+
+    return results;
 }
